cb8f731a72b7bdf5821c40fba15d884ec0123497
rmlgc <rommelgarcia4b@gmail.com>
2022-12-12 11:22:46 +0000

    deploying GH-page

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require("path");
+const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+const base = mode === "production" ? "/" + path.basename(process.cwd()) + "/" : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [vue()],
   resolve: {
     alias: {
